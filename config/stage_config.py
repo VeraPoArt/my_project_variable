dc5ae4135c89b7840a7d6f0c238bd5aac3bf0918
VeraPoArt <VeraPoArt@users.noreply.github.com>
2025-01-01 20:36:13 +0000

    deploy: 201bbff2049f26ec8230b3ce369bc1f32fa48042

--- config/stage_config.py
+++
@@ -1,6 +0,0 @@
-config = {
-    "base_url": "https://thinking-tester-contact-list.herokuapp.com",
-    "api_base_url": "https://thinking-tester-contact-list.herokuapp.com",
-    "username": "testuser@example.com",
-    "password": "Password123"
-}
--- tests/test_example.py
+++
@@ -1,3 +0,0 @@
-def test_open_page(page, config):
-    page.goto(config["base_url"])
-    assert page.get_by_role("heading", name="Example Domain").is_visible()
